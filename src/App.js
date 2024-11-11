ec642120f375b304fdde0451f07a6275f2940951
Hoanglee-dev <levuhoangks@gmail.com>
2024-11-11 22:50:12 +0700

    fix app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import ContextDataTable from "./components/context/ContextDataTable";
 import TableManagement from "./components/TableManagement/TableManagement";
 import FeedbackContext from "../src/components/context/FeedbackContext";
 import FeedbackTable from "../src/components/FeedBack/FeedbackTable";
-import {Home} from "./components/partial/Home";
+import { Home } from "./components/partial/Home";
 import { Header } from "./components/partial/Header";
-import {CreatePost} from "./components/posts/server/admin/CreatePost"
-import {ListPost} from "./components/posts/client/ListPost"
-import {PostManagement} from "./components/posts/server/admin/PostManagement"
-import {EditPost} from "./components/posts/server/admin/EditPost"
+import { CreatePost } from "./components/posts/server/admin/CreatePost";
+import { ListPost } from "./components/posts/client/ListPost";
+import { PostManagement } from "./components/posts/server/admin/PostManagement";
+import { EditPost } from "./components/posts/server/admin/EditPost";
 import DashBoard from "./components/posts/server/admin/DashBoard";
 
 // import TableManagement from "./TableManagement";
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} >
+        <Route path="/" element={<Home />}>
           <Route path="header" element={<Header />} />
           <Route path="create" element={<CreatePost />} />
           <Route path="listpost" element={<ListPost />} />
@@ -25,7 +25,7 @@ function App() {
           <Route path="/edit/:id" element={<EditPost />} />
           <Route path="dash" element={<DashBoard />} />
         </Route>
-        {/* <Route path="admin" element={<Admin />}>
+        <Route path="staff" element={<Staff />}>
           <Route
             path="sale"
             element={
@@ -42,7 +42,7 @@ function App() {
               </FeedbackContext>
             }
           />
-        </Route> */}
+        </Route>
       </Routes>
     </>
   );
