73865b86fa5721a44caa10bbd3f06281ea05157a
huynhnhule2004 <huynhnhule2004@gmail.com>
2024-11-13 09:23:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './components/pages/client/Home';
+import {Login} from "../src/components/user/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
         {/* <Route path="admin" element={<Admin />}>
           <Route
             path="sale"
