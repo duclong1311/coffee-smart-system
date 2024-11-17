7827d99e12f2e27d9af9023c465388f2f757082c
Nguyễn Thành Nhân <99931765+ThanhNhan2002-dev@users.noreply.github.com>
2024-11-18 00:11:44 +0700

    sua profile

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -34,18 +34,19 @@ export function Profile() {
                         {/* Container for user information and profile image */}
                         <div className="flex flex-col md:flex-row-reverse gap-6 items-start md:items-center mb-6">
                             {/* Profile image */}
-                            <div className="w-[250px] h-[300px] bg-[url('https://cdn.eva.vn/upload/3-2021/images/2021-07-31/182636312_1294694704259617_8777969660546104509_n-down-1627714443-420-width800height1067.jpg')] bg-cover bg-center bg-no-repeat border-4 border-[#333] rounded-lg"></div>
+                            <div className="w-[250px] h-[300px] bg-[url('https://cdn.eva.vn/upload/3-2021/images/2021-07-31/182636312_1294694704259617_8777969660546104509_n-down-1627714443-420-width800height1067.jpg')] 
+                            bg-cover bg-center bg-no-repeat border-4 border-[#333] rounded-lg relative right-[100px] top-[0px]"></div>
                             {/* User information */}
-                            <div className="mt-[10px] mx-auto w-[80%]">
-                                <p className="mb-2 font-semibold">Tên Đăng Nhập: <span className="font-normal">{user.username}</span></p>
-                                <p className="mb-2 font-semibold">Mật Khẩu: <span className="font-normal">{user.password}</span></p>
-                                <p className="mb-2 font-semibold">Họ và Tên: <span className="font-normal">{user.fullName}</span></p>
-                                <p className="mb-2 font-semibold">Địa Chỉ: <span className="font-normal">{user.address}</span></p>
-                                <p className="mb-2 font-semibold">Giới Tính: <span className="font-normal">{user.gender}</span></p>
-                                <p className="mb-2 font-semibold">Số Điện Thoại: <span className="font-normal">{user.phone}</span></p>
-                                <p className="mb-2 font-semibold">Ngày Sinh: <span className="font-normal">{user.dob}</span></p>
-                                <p className="mb-2 font-semibold">Lương: <span className="font-normal">{user.salary}</span></p>
-                                <p className="mb-2 font-semibold">Vị Trí: <span className="font-normal">{user.position}</span></p>
+                            <div className="mt-[10px] mx-auto w-[80%] relative left-[100px] top-[0px]">
+                            <p className="mb-2 font-semibold" style={{ width: '300px' }}>Tên Đăng Nhập: <span className="font-normal">{user.username}</span></p>
+                                <p className="mb-2 font-semibold" style={{ width: '300px' }}>Mật Khẩu: <span className="font-normal">{user.password}</span></p>
+                                <p className="mb-2 font-semibold" style={{ width: '300px' }}>Họ và Tên: <span className="font-normal">{user.fullName}</span></p>
+                                <p className="mb-2 font-semibold" style={{ width: '300px' }}>Địa Chỉ: <span className="font-normal">{user.address}</span></p>
+                                <p className="mb-2 font-semibold"style={{ width: '300px' }} >Giới Tính: <span className="font-normal">{user.gender}</span></p>
+                                <p className="mb-2 font-semibold" style={{ width: '300px' }}>Số Điện Thoại: <span className="font-normal">{user.phone}</span></p>
+                                <p className="mb-2 font-semibold" style={{ width: '300px' }}>Ngày Sinh: <span className="font-normal">{user.dob}</span></p>
+                                <p className="mb-2 font-semibold" style={{ width: '300px' }}>Lương: <span className="font-normal">{user.salary}</span></p>
+                                <p className="mb-2 font-semibold" style={{ width: '300px' }}>Vị Trí: <span className="font-normal">{user.position}</span></p>
                             </div>
                         </div>
 
