4b9cf5f85b0a89fda96cc14259326a6ef4d44216
huynhnhule2004 <huynhnhule2004@gmail.com>
2024-11-18 20:32:47 +0700

    Fix lỗi hiện thông báo

--- src/components/user/ProtectedRoute.js
+++ src/components/user/ProtectedRoute.js
@@ -1,9 +1,13 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { checkRole } from "./auth";
+import { useEffect, useState } from "react";
 
 export const ProtectedRoute = ({ children, requiredRole }) => {
     const hasAccess = checkRole(requiredRole);
+    const navigate = useNavigate();
+    const [redirecting, setRedirecting] = useState(false); // Kiểm tra trạng thái chuyển hướng
+    const [toastShown, setToastShown] = useState(false); // Kiểm tra khi nào toast được hiển thị
 
     if (!hasAccess) {
         // Kiểm tra xem đã hiển thị thông báo chưa trong sessionStorage
